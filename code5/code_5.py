522733d2ac8c02c2662d4511825227a70d20f362
tae0606il <128006391+tae0606il@users.noreply.github.com>
2023-05-04 10:30:12 +0900

    Add files via upload

---
+++ code5/code_5.py
@@ -0,0 +1,12 @@
+print("#" * 23)
+print("# 이름, 나이, 답변 앱 #")
+print("#" * 23)
+
+yu_name + str(input("이름:"))
+yu_age + int(input("나이입력: "))
+
+if yu_age <= 25 :
+    print("와우!!! 프로그래밍을 완벽하게 배울수 있는 나이입니다.!")
+else:
+    print("포기하기에는 아직 늦지 않았습니다.")
+print("/n")
